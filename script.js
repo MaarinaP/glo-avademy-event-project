1726a00a1dbb1add1bac819a74945c48436f7b30
Marina Pavlova <marinap.learning@gmail.com>
2022-02-16 16:35:25 +0000

    Add event listener to the button called button

--- script.js
+++ script.js
@@ -1,3 +1,21 @@
+const buttonIn = document.getElementById("e_btn");
+
+const btn = document.getElementById("btn");
+const text = document.getElementById("text");
+const square = document.getElementById("square");
+
+const colorChange = function() {
+    square.style.backgroundColor = text.value;
+};
+
+//поменять display свойства для кнопки в круге
+buttonIn.style.display = "none";
+
+//смена цвета квадрата
+btn.addEventListener("click", colorChange);
+
+
+
 
 
 
