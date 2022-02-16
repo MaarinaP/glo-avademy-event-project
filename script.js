2dee9a82a913e3404b876e5635c7089acaa91a18
Marina Pavlova <marinap.learning@gmail.com>
2022-02-16 17:32:28 +0000

    Complete lesson 11

--- script.js
+++ script.js
@@ -4,8 +4,19 @@ const btn = document.getElementById("btn");
 const text = document.getElementById("text");
 const square = document.getElementById("square");
 
+const range = document.getElementById("range");
+const span = document.getElementById("range-span");
+const circle = document.getElementById("circle");
+range.step = 10;
+
 const colorChange = function() {
     square.style.backgroundColor = text.value;
+};
+
+const rangeChange = function(event) {
+    span.textContent = event.target.value;
+    circle.style.width = event.target.value + "%";
+    circle.style.height = circle.style.width;
 };
 
 //поменять display свойства для кнопки в круге
@@ -13,6 +24,11 @@ buttonIn.style.display = "none";
 
 //смена цвета квадрата
 btn.addEventListener("click", colorChange);
+
+//изменение величины круга
+range.addEventListener('input', rangeChange);
+
+
 
 
 
